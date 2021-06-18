279053fdf5719569e6dc46fb17bdee95764d4e5c
rahul prajapati <rahul.prajapati@ilogixinfotech.com>
2021-06-18 19:30:24 +0530

    config reverted

--- src/app/providers/constant/constant.ts
+++ src/app/providers/constant/constant.ts
@@ -2,8 +2,8 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class AppConstants  {
-    apiurl : string = 'http://vtiger.local/sunergy/phoneapi/';
-    vturl : string = 'http://vtiger.local/sunergy/';
+    apiurl : string = 'https://devl06.borugroup.com/sunergy/phoneapi/';
+    vturl : string = 'https://devl06.borugroup.com/sunergy/';
     getApiUrl() {
         return this.apiurl;
     }
